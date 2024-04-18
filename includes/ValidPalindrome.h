947d385b20d6ce7638238ff8cfed715cc70af27e
phosphataz <raoufilokou@gmail.com>
2024-04-18 17:17:31 +0100

    setup cmake and test

---
+++ includes/ValidPalindrome.h
@@ -0,0 +1,10 @@
+
+#ifndef VALID_PALINDROM_H
+#define VALID_PALINDROM_H
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(string s);
+};
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../includes/ValidPalindrome.h"
+
+
+
+
+int main(){
+    Solution s;
+    std::cout << "Result : " << std::boolalpha << s.isPalindrome("A man, a plan, a canal: Panama") << std::endl;
+    return 0;
+}
